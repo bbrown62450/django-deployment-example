2dd364592a278fa1af9075f69c9e88c2f076683d
Beau Brown <beaubrown@Diagnotess-MacBook-Pro.local>
2022-03-10 16:12:25 -0500

    added allowed host

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-u577owzp0%ni*#h^aozrbr(q%-byv+ab6fhwq&en5c$y%1kjw8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bbrown62450.pythonanywhere.com']
 
 
 # Application definition
